8437411169f0f2b777e1ba82d913fd24211a1b59
Rakheros <denis.boiseaux@gmail.com>
2025-07-18 19:19:34 +0200

    First Commit

---
+++ src/main/java/net/rakheros/horseequipment/HorseEquipmentDataGenerator.java
@@ -0,0 +1,11 @@
+package net.rakheros.horseequipment;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HorseEquipmentDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
